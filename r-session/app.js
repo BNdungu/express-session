dc0145cc092c380c6c66f16b363286edd9f8d02e
BNdungu <bryannganga909@gmail.com>
2023-12-27 13:18:48 +0300

    -

--- r-session/app.js
+++ r-session/app.js
@@ -7,6 +7,11 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
+const RedisStore = connectRedis(session) // wire up our connect redis package to the express-session
+
+//configure redis
+ 
+
 app.listen(port, () => {
     console.log(`Server started listenningi on Port ${port}`)
 })
