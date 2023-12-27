2a94a6a74e76b07d14747035baf30e9610a3a454
BNdungu <bryannganga909@gmail.com>
2023-12-27 15:34:05 +0300

    -

--- r-session/app.js
+++ r-session/app.js
@@ -6,11 +6,33 @@ const connectRedis = require('connect-redis')
 const app = express()
 const port = process.env.PORT || 3000
 
+// If running behind a reverse proxy
+app.set('trust proxy', 1)
 
 const RedisStore = connectRedis(session) // wire up our connect redis package to the express-session
 
 //configure redis
+const redisClient = redis.createClient({
+    port: 6379,
+    host: 'localhost'
+})
+
+app.use(session({
+    store: new RedisStore({client: redisClient}),
+    secret: 'secret',
+    saveUninitialized: false,
+    resave: false,
+    cookie:{
+        secure: false,
+        httpOnly: true,
+        maxAge: 1000 * 60 * 30 
+    }
+}))
  
+
+app.post('/login', (req,res) => {
+    const {email, password} = req
+})
 
 app.listen(port, () => {
     console.log(`Server started listenningi on Port ${port}`)
