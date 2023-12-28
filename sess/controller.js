cc19bb4cc812e361cbd7a348128e3402b2a65621
BNdungu <bryannganga909@gmail.com>
2023-12-28 19:28:55 +0300

    -

--- sess/controller.js
+++ sess/controller.js
@@ -4,10 +4,20 @@ const get = () => {
         if(sess.username){
             res.write(`<h1>Welcome ${sess.username}</h1><br>`)
             res.write(`<h3>This is the homepage</h3>`)
+            res.status(200).json({status: 'logged in'})
         }
     }
 }
 
 const login = () => {
-    const sess
+    const sess = req.session
+    const {username, password} = req.body
+    sess.username = username
+    sess.password = password
+    res.send('succes')
+}
+
+module.exports = {
+    get,
+    login
 }
